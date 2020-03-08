fa23e73476a3978b2317f2930b15ae8d4a07ce66
georgetegral <georgetegral@gmail.com>
2020-03-08 10:57:27 -0600

    implementing EntityFramework in memory with EscuelaContext

---
+++ Models/EscuelaContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace platzi_asp_net_core.Models
+{
+    public class EscuelaContext: DbContext
+    {
+        public DbSet <Escuela> Escuelas {get;set;}
+        public DbSet <Asignatura> Asignaturas {get;set;}
+        public DbSet <Alumno> Alumnos {get;set;}
+        public DbSet <Curso> Cursos {get;set;}
+        public DbSet <Evaluación> Evaluaciones {get;set;}
+        public EscuelaContext (DbContextOptions<EscuelaContext> options): base(options)
+        {
+
+        }
+    }
+}
