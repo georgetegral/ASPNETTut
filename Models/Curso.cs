598ac4b5ea8531580e574e01f8a5ac053e222ac0
georgetegral <georgetegral@gmail.com>
2020-03-16 17:27:35 -0600

    Changes to model

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 
 namespace platzi_asp_net_core.Models
 {
-    public class Curso:ObjetoEscuelaBase
+    public class Curso : ObjetoEscuelaBase
     {
         public TiposJornada Jornada { get; set; }
-        public List<Asignatura> Asignaturas{ get; set; }
-        public List<Alumno> Alumnos{ get; set; }
-
+        public List<Asignatura> Asignaturas { get; set; }
+        public List<Alumno> Alumnos { get; set; }
         public string Dirección { get; set; }
-
+        public string EscuelaId { get; set; }
+        public Escuela Escuela { get; set; }
     }
 }
